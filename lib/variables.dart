71625fbd5d86d6e8b7c2eaaed43029d36ecde5c4
Rashmi Jayawardhana <155569390+rashmiJayawardhana@users.noreply.github.com>
2025-04-04 00:22:58 +0530

    Variables

--- lib/variables.dart
+++ lib/variables.dart
@@ -5,11 +5,17 @@ void main(List<String> args){
   final firstName = "RJ";
   print(firstName);
 
+  final String lastName = "Minduli";
+
   const pi = 3.145;
   print(pi);
 
+  const int students = 5;
+
   int age = 24;
   print(age);
+
+  int? teachers; // Can be nullable - ?
 
   double price = 120.0;
   print(price);
